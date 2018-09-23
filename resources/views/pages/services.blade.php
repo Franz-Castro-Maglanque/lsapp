ab88357af382be7f4a3af7c20c33518cf2c9da27
Franz-Castro-Maglanque <40532587+Franz-Castro-Maglanque@users.noreply.github.com>
2018-09-23 15:32:39 +0800

    update_part5

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts/app')
     @section('content')
     <h1>Welcome to the Services Page</h1>
-    </ul>
+    </ul class="list-group">
         @foreach($services as $service)
-            <li>{{$service}}</li>
+            <li class="list-group-item">{{$service}}</li>
         @endforeach
     </ul>
     @endsection
